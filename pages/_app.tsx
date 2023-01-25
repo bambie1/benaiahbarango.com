cf3fac3b760a6d2be9a08678434535f45b27de7c
Benaiah Barango <benaiah.barango@fullscript.com>
2023-01-24 22:50:11 -0500

    implement design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "@next/font/google";
+import PlausibleProvider from "next-plausible";
+
+const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="benaiahbarango.com">
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    </PlausibleProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,68 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const PROJECTS = [
+  {
+    title: "SoPlugged",
+    description: "A website to find black-owned businesses in Canada",
+    link: {
+      title: "soplugged.com",
+      href: "https://www.soplugged.com",
+    },
+  },
+  {
+    title: "Treats Royale",
+    description: "A Toronto-based bakery business built using Shopify's API",
+    link: {
+      title: "treatsroyale.ca",
+      href: "https://www.treatsroyale.ca",
+    },
+  },
+];
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Benaiah Barango | Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+      <main className="flex tracking-tighter w-full flex-1 flex-col items-center justify-center px-4 lg:px-20 text-center">
+        <h1 className="text-4xl lg:text-6xl font-bold">Benaiah Barango</h1>
+        <p className="text-gray-600 font-light text-lg lg:text-2xl mt-2">
+          Javascript Engineer @ Fullscript
         </p>
+        <hr />
+        <div className="mt-10">
+          <h2 className="text-2xl lg:text-4xl font-semibold">WORK</h2>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <ul className="grid gap-10 mt-6">
+            {PROJECTS.map((project) => (
+              <li key={project.title}>
+                <h3 className="text-lg lg:text-xl font-light uppercase tracking-normal">
+                  {project.title}
+                </h3>
+                <p>{project.description}</p>
+                <a
+                  href={project.link.href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="underline text-gray-500"
+                >
+                  {project.link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <footer className="flex py-4 w-full items-center justify-center border-t border-gray-100">
+        Copyright. 2023
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
