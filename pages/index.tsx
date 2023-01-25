17f9e96b343df0c8b4a7b7b5627244d850513e88
Benaiah Barango <benaiah.barango@fullscript.com>
2023-01-25 07:22:05 -0500

    text updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
+import { FC } from "react";
+
+import SVGLinks from "../src/components/SVGLinks";
 
 const PROJECTS = [
   {
@@ -22,46 +26,77 @@ const PROJECTS = [
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
         <title>Benaiah Barango | Portfolio</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main className="flex tracking-tighter w-full flex-1 flex-col items-center justify-center px-4 lg:px-20 text-center">
-        <h1 className="text-4xl lg:text-6xl font-bold">Benaiah Barango</h1>
-        <p className="text-gray-600 font-light text-lg lg:text-2xl mt-2">
-          Javascript Engineer @ Fullscript
-        </p>
-        <hr />
-        <div className="mt-10">
-          <h2 className="text-2xl lg:text-4xl font-semibold">WORK</h2>
+        <div>
+          <h1 className="text-4xl lg:text-6xl font-bold">Benaiah Barango</h1>
+          <p className="text-gray-500 font-light text-lg lg:text-2xl mt-2">
+            Javascript Engineer @ Fullscript
+          </p>
 
-          <ul className="grid gap-10 mt-6">
-            {PROJECTS.map((project) => (
-              <li key={project.title}>
-                <h3 className="text-lg lg:text-xl font-light uppercase tracking-normal">
-                  {project.title}
-                </h3>
-                <p>{project.description}</p>
-                <a
-                  href={project.link.href}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline text-gray-500"
-                >
-                  {project.link.title}
-                </a>
+          <SVGLinks />
+
+          <section className="mt-5">
+            <p>Looking for a website?</p>
+            <p className="text-gray-500 lg:text-lg">
+              Reach out on{" "}
+              <Link
+                className="underline"
+                href="https://www.soplugged.com/pro"
+                target="_blank"
+                rel="norefferer"
+              >
+                SoPlugged Pro
+              </Link>
+            </p>
+          </section>
+        </div>
+
+        <hr className="w-full max-w-md mt-10 mb-6" />
+
+        <div>
+          <div className="flex flex-wrap gap-2">
+            <p>Currently maintaining:</p>
+            <ul>
+              <li>
+                <ProjectLink href="soplugged.com" title="SoPlugged" />
               </li>
-            ))}
-          </ul>
+            </ul>
+          </div>
+
+          <div className="flex items-center justify-center flex-wrap gap-2">
+            <p>Past projects:</p>
+            <ul>
+              <li>
+                <ProjectLink href="treatsroyale.ca" title="Treats Royale" />
+              </li>
+            </ul>
+          </div>
         </div>
       </main>
 
-      <footer className="flex py-4 w-full items-center justify-center border-t border-gray-100">
-        Copyright. 2023
+      <footer className="flex py-4 w-full items-center justify-center text-sm text-gray-400">
+        <p>Copyright. {new Date().getFullYear()}</p>
       </footer>
     </div>
+  );
+};
+
+const ProjectLink = ({ href, title }: { href: string; title: string }) => {
+  return (
+    <Link
+      href={`https://www.${href}`}
+      title={title}
+      className="underline text-gray-500 font-light"
+      target="_blank"
+    >
+      {href}
+    </Link>
   );
 };
 
